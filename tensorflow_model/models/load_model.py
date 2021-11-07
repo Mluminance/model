0a2ba14734ec0d990b5376b7e9b6fe2c8e4c0a4f
Karim Daw <karimdaw91@gmail.com>
2021-11-07 02:01:45 +0100

    fix names and load model from json

--- tensorflow_model/models/load_model.py
+++ tensorflow_model/models/load_model.py
@@ -1,6 +1,23 @@
-json_file = open('models/DA_dnn.json', 'r')
+from flask import Flask
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import os
+import json
+import sys
+
+#path = os.path.join(sys.path[0], "log\DA_dnn_10-4.json")
+#print(path)
+
+json_file = open('DA_dnn_10-4.json', 'r')
+print(json_file)
+
 loaded_model_json = json_file.read()
+print(loaded_model_json)
 json_file.close()
-loaded_model = model_from_json(loaded_model_json)
- 
-loaded_model.load_weights('models/DA_dnn.h5'))
+
+loaded_model = tf.keras.models.model_from_json(loaded_model_json)
+
+
+loaded_model.load_weights('DA_dnn_10-4.h5')
+loaded_model.summary() 
