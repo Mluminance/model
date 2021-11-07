350a84fd448ae47a3c99044a3f8d82a1fca2b4da
Karim Daw <karimdaw91@gmail.com>
2021-11-07 11:23:31 +0100

    fix orientation according to y up

--- tensorflow_model/models/app.py
+++ tensorflow_model/models/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import ghhops_server as hs
 import rhino3dm as rdm
+import math
 
 
 
@@ -16,7 +17,7 @@ hops = hs.Hops(app)
     description="Predicts the UDI of a given room object",
     #icon="learncarbon_logo_without_text.png",
     inputs=[
-        hs.HopsNumber("roomBoundary", "Room Boundary Curve", "Closed curve representing the room boundary"),
+        hs.HopsCurve("roomBoundary", "Room Boundary Curve", "Closed curve representing the room boundary"),
         hs.HopsNumber("windowWidth", "Window Width", "Width of Window"),
         hs.HopsNumber("windowHeight", "Window Height", "Height of Window"),
     ],
@@ -27,10 +28,8 @@ hops = hs.Hops(app)
 def getUDIPrediction(roomBoundary, windowWidth,windowHeight):
 
     #convert to polyline to be able to measure distances
-
-    #myPolyline = roomBoundary.ToPolyline()
-    # my points
-    """
+    myPolyline = roomBoundary.ToPolyline()
+    
     point0: rdm.Point3d = myPolyline[0]
     point1: rdm.Point3d = myPolyline[1]
     point2: rdm.Point3d = myPolyline[2]
@@ -41,14 +40,43 @@ def getUDIPrediction(roomBoundary, windowWidth,windowHeight):
     sideB = point1.DistanceTo(point2)
     sideC = point2.DistanceTo(point3)
     sideD = point3.DistanceTo(point0)
-    """
 
     # calculating orientation
     # get first vector
-    #fVector: rdm.Vector3d = point1 - point0
-    
-    print("this is a test")
-    
+    vX = point1.X - point0.X
+    vY = point1.Y - point0.Y
+    vZ = point1.Z - point0.Z 
+
+    fVector = rdm.Vector3d(vX,vY,vZ)
+
+    #compute north vector
+    nVector = rdm.Vector3d(0.0,0.0,0.0)
+
+    # calculate angle according to x access
+    # based on https://stackoverflow.com/questions/42258637/how-to-know-the-angle-between-two-vectors
+    radAngle = math.atan2(fVector.X-nVector.X, fVector.Y-nVector.Y) - math.pi*0.5
+    degAngle = abs(math.degrees(radAngle))
+
+    #print(degAngle)
+    #print((radAngle))
+
+    if degAngle <= 180 and degAngle >= 90:
+        myAngle = abs(degAngle - 180)
+    elif degAngle <= 90 and degAngle >= 0 and radAngle < 0:
+        myAngle = abs(degAngle - 180)
+    elif degAngle <= 90 and degAngle >= 0 and radAngle > 0:
+        myAngle = degAngle + 180
+    elif degAngle  <= 270 and degAngle >= 180:
+        myAngle = (360 - degAngle) + 180
+
+    print("this is my starting angle")
+    print(degAngle)
+    #print(radAngle)
+    print("this is my orientation")
+    print(myAngle)
+
+    #print("this is a test")
+
     return "hello Karim"
 
 if __name__ == "__main__":
